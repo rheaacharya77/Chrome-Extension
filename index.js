18c113067793a766ce5b1a668d506f74bdab232c
ria.acharya07 <ria.acharya07@gmail.com>
2022-02-28 21:09:27 +0545

    chrome api

--- index.js
+++ index.js
@@ -11,20 +11,15 @@ if(leadsFromLocalStorage){
     render(myLeads);
 }
 
-const tabs = [
-{url: "www.linkedin.com/riya-acharya"}
-]
 
 tabBtn.addEventListener("click",function(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        // since only one tab should be active and in the current window at once
-        // the return variable should only have one entry
-        let activeTab = tabs[0]
-        let activeTabId = activeTab.id // or do whatever you need
-    });
+    chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
     myLeads.push(tabs[0].url);
     localStorage.setItem("myLeads",JSON.stringify(myLeads));
-    render(myLeads);
+    render(myLeads); 
+  })
+
+   
 })
 
 function render(leads){
