0805a8e398562d659e33966e4cdbfd15be020c73
ria.acharya07 <ria.acharya07@gmail.com>
2022-02-28 19:49:09 +0545

    code clean

--- index.js
+++ index.js
@@ -9,34 +9,34 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
+function render(leads){
+    let listItems = "";
+    for(let i=0;i< leads.length;i++){
+        listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'> 
+            ${leads[i]}  
+         </a>
+         </li>
+         `;
+    }
+    ulEl.innerHTML = listItems;
+    }
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads",JSON.stringify(myLeads));
-    renderLeads();
+    render(myLeads);
 
 });
 
 deleteBtn.addEventListener("dblclick",function(){
     localStorage.clear();
     myLeads = [];
-    renderLeads();
+    render(myLeads);
 
 })
 
-function renderLeads(){
-let listItems = "";
-for(let i=0;i< myLeads.length;i++){
-    listItems += `
-    <li>
-        <a target='_blank' href='${myLeads[i]}'> 
-        ${myLeads[i]}  
-     </a>
-     </li>
-     `;
-}
-ulEl.innerHTML = listItems;
-}
