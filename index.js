a50ce94398934d03c77a689c1fe4c5451328372b
ria.acharya07 <ria.acharya07@gmail.com>
2022-02-28 20:53:10 +0545

    save tab url

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
-
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage;
     render(myLeads);
 }
+
+const tabs = [
+{url: "hsjkshdj"}
+]
+
+tabBtn.addEventListener("click",function(){
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads",JSON.stringify(myLeads));
+    render(myLeads);
+})
+
 function render(leads){
     let listItems = "";
     for(let i=0;i< leads.length;i++){
