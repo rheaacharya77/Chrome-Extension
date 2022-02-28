3bd19ecbdfcb1584a9566e1b46faafd5728ab0db
ria.acharya07 <ria.acharya07@gmail.com>
2022-02-28 21:00:07 +0545

    new tab url

--- index.js
+++ index.js
@@ -12,10 +12,16 @@ if(leadsFromLocalStorage){
 }
 
 const tabs = [
-{url: "hsjkshdj"}
+{url: "www.linkedin.com/riya-acharya"}
 ]
 
 tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // since only one tab should be active and in the current window at once
+        // the return variable should only have one entry
+        let activeTab = tabs[0]
+        let activeTabId = activeTab.id // or do whatever you need
+    });
     myLeads.push(tabs[0].url);
     localStorage.setItem("myLeads",JSON.stringify(myLeads));
     render(myLeads);
